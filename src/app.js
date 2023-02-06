b4beda534c2fe15b1e0cf9c05e3b1a4e803286c5
나다운 <skekfl12@gitauto.com>
2023-02-06 18:41:13 +0900

    Setting basic

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import exportDefaultTest, { add, sub } from "./module";
+
+console.log(exportDefaultTest(20, 10));
+console.log(add(20, 10));
+console.log(sub(20, 10));
---
+++ src/module.js
@@ -0,0 +1,8 @@
+/** Multiplication */
+export const mul = (a, b) => a * b;
+/** Addition */
+export const add = (a, b) => a + b;
+/** Subtraction */
+export const sub = (a, b) => a - b;
+
+export default mul;
