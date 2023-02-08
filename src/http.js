ae712338641ab0bccc0e28175a4aa874c3080e9b
나다운 <skekfl12@gitauto.com>
2023-02-08 15:49:41 +0900

    remove set

--- src/http.js
+++
@@ -1,9 +0,0 @@
-const ajax = new XMLHttpRequest();
-
-/** Common XMLHttpRequest */
-export const getResponse = (method, url, isAsync) => {
-  ajax.open(method, url, isAsync);
-  ajax.send();
-
-  return JSON.parse(ajax.response);
-};
