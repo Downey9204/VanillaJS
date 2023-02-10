8e2ac2d9c84a3c4aad1acb6627d01938ec505c37
나다운 <skekfl12@gitauto.com>
2023-02-10 11:39:30 +0900

    Refactoring app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,46 @@
 import ajax from "./http";
 
-const app = document.querySelector("#root");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/@page.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-const newsFeed = ajax("GET", NEWS_URL.replace("@page", "1"), false);
 
+const app = document.querySelector("#root");
 const feed = document.createElement("ul");
-for (let i = 0; i < newsFeed.length; i++) {
-  const item = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${newsFeed[i].id}`;
-  a.textContent = newsFeed[i].title;
+const content = document.createElement("section");
 
-  item.appendChild(a);
-  feed.appendChild(item);
+/** Init when load is complete */
+window.addEventListener("load", init);
+/** Display content when hash is changed */
+window.addEventListener("hashchange", createContent);
+
+/** Init */
+function init() {
+  const newsFeed = ajax("GET", NEWS_URL.replace("@page", "1"), false);
+  createFeed(newsFeed);
+
+  app.appendChild(content);
+  app.appendChild(feed);
 }
 
-const content = document.createElement("section");
-window.addEventListener("hashchange", () => {
+/** Create Feed */
+function createFeed(newsFeed) {
+  for (let i = 0; i < newsFeed.length; i++) {
+    const item = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = `#${newsFeed[i].id}`;
+    a.textContent = newsFeed[i].title;
+
+    item.appendChild(a);
+    feed.appendChild(item);
+  }
+}
+
+/** Create Content */
+function createContent() {
   const id = location.hash.substring(1);
   const newsContent = ajax("GET", CONTENT_URL.replace("@id", id), false);
 
   const title = document.createElement("h1");
   title.textContent = newsContent.title;
 
-  content.appendChild(title);
-});
-
-app.appendChild(feed);
-app.appendChild(content);
+  content.innerHTML = title.innerHTML;
+}
