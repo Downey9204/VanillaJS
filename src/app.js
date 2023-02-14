68954a473f3e21e0ae59af048f9763ec7457e5f0
나다운 <skekfl12@gitauto.com>
2023-02-14 10:47:56 +0900

    Refactoring v3

--- src/app.js
+++ src/app.js
@@ -28,15 +28,37 @@ function setRoute() {
     createFeed();
   } else if (path.includes("detail/")) {
     createContent();
-  } else console.log("404 Error");
+  } else console.error("404 Error");
 }
 
 /** Create Feed */
 function createFeed() {
   const page = String(store.currentPage);
   const newsFeed = ajax("GET", NEWS_URL.replace("@page", page), false);
-  console.log(newsFeed.length);
-  const newsList = ["<ul>"];
+  let template = `
+    <section>
+      <h1>News Feed</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <span>
+          <a href="#/page/{{__prev_page__}}">
+            이전 페이지
+          </a>
+        </span>
+        <span>
+          <a href="#/page/{{__next_page__}}">
+            다음 페이지
+          </a>
+        </span>
+      </div>
+    </section>
+  `;
+  const newsList = [];
+  const prev = store.currentPage > 1 ? store.currentPage - 1 : 1;
+  const next = newsFeed.length ? store.currentPage + 1 : store.currentPage;
+
   if (newsFeed.length) {
     for (let i = 0; i < NEWS_COUNT_PER_PAGE; i++) {
       newsList.push(`
@@ -48,38 +70,29 @@ function createFeed() {
       `);
     }
   }
-  newsList.push("</ul>");
-  newsList.push(`
-    <div>
-      <span>
-        <a 
-          href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}"
-        >
-          이전
-        </a>
-      </span>
-      <span>
-        <a 
-          href="#/page/${
-            newsFeed.length ? store.currentPage + 1 : store.currentPage
-          }"
-        >
-          다음
-        </a>
-      </span>
-    </div>
-  `);
 
-  app.innerHTML = newsList.join("");
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace("{{__prev_page__}}", prev);
+  template = template.replace("{{__next_page__}}", next);
+
+  app.innerHTML = template;
 }
 
 /** Create Content */
 function createContent() {
   const id = location.hash.substring(9);
   const newsContent = ajax("GET", CONTENT_URL.replace("@id", id), false);
-
-  app.innerHTML = `
-    <h1>${newsContent.title}</h1>
-    <a href="#/page/${store.currentPage}">To List</a>
+  let template = `
+    <section>
+      <h1>{{__content_title__}}</h1>
+      <a href="#/page/{{__current_page__}}">
+        To List
+      </a>
+    </section>
   `;
+
+  template = template.replace("{{__content_title__}}", newsContent.title);
+  template = template.replace("{{__current_page__}}", store.currentPage);
+
+  app.innerHTML = template;
 }
